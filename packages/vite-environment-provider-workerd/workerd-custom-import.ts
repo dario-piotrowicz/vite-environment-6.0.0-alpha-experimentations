ba2f6024b7b28934a9ed2e784447297736d46b8e
Dario Piotrowicz <dario@cloudflare.com>
2024-05-31 16:58:51 +0100

    remove extra console.logs

--- packages/vite-environment-provider-workerd/workerd-custom-import.ts
+++ packages/vite-environment-provider-workerd/workerd-custom-import.ts
@@ -1,5 +1,4 @@
 module.exports = async (path: string) => {
-  console.log(`\x1b[34mcustom import on ${path}\x1b[0m`);
   try {
     const result = await import(path);
     // TODO: investigate why we need the following
@@ -8,7 +7,6 @@ module.exports = async (path: string) => {
       ...(result.default ?? {}),
     };
   } catch (e) {
-    console.log(`\x1b[31m custom import error \x1b[0m`, e);
     // return the following string instead of a module to make the potential issue slightly clearer
     return '__CUSTOM_IMPORT__module_not_loaded__';
   }
