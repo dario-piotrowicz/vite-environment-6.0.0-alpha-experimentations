a57739ced5ea4f075bf43407f5ff7d9ef8b3ff0e
James Opstad <jopstad@cloudflare.com>
2024-07-29 14:03:25 +0100

    Removed commented code

--- packages/vite-environment-provider-node-vm/src/index.ts
+++ packages/vite-environment-provider-node-vm/src/index.ts
@@ -131,49 +131,6 @@ async function createNodeVmDevEnvironment(
     eventEmitter,
   });
 
-  // const script = `
-  //   let _moduleRunner;
-  //   async function getModuleRunner() {
-  //     if (_moduleRunner) return _moduleRunner;
-  //     const { ModuleRunner } = await import("vite/module-runner");
-  //     _moduleRunner = new ModuleRunner(
-  //         {
-  //           root: config.root,
-  //           transport: {
-  //             fetchModule: async (...args) => devEnv.fetchModule(...args),
-  //           },
-  //           hmr: {
-  //             connection: {
-  //               isReady: () => true,
-  //               onUpdate(callback) {
-  //                 eventEmitter.on("message", (event) => {
-  //                   callback(JSON.parse(event));
-  //                 });
-  //               },
-  //               send(message) {
-  //                 eventEmitter.emit("message",JSON.stringify(message));
-  //               },
-  //             },
-  //           },
-  //         },
-  //         {
-  //           runInlinedModule: async (context, transformed, id) => {
-  //             const codeDefinition = \`'use strict';async (\${Object.keys(context).join(
-  //               ',',
-  //             )})=>{{\`;
-  //             const code = \`\${codeDefinition}\${transformed}\n}}\`;
-  //             const fn = eval(code, id);
-  //             await fn(...Object.values(context));
-  //             Object.freeze(context.__vite_ssr_exports__);
-  //           },
-  //           async runExternalModule(filepath) {
-  //             return import(filepath);
-  //           },
-  //         },
-  //     );
-  //     return _moduleRunner;
-  //   }
-
   const script = `
     let _moduleRunner;
     async function getModuleRunner() {
@@ -223,9 +180,7 @@ async function createNodeVmDevEnvironment(
   return devEnv as DevEnvironment;
 }
 
-function createSimpleHotChannel(
-  eventEmitter: EventEmitter,
-): HotChannel {
+function createSimpleHotChannel(eventEmitter: EventEmitter): HotChannel {
   let hotDispose: (() => void) | undefined;
 
   const hotEventListenersMap = new Map<
